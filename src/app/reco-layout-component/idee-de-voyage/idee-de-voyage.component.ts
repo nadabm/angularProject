5e74c0fa151edacca7305a28ac240880f532efd8
ferdaous <echahbouniferdaous@gmail.com>
2023-12-25 22:27:39 +0100

    recommnedation

--- src/app/reco-layout-component/idee-de-voyage/idee-de-voyage.component.ts
+++ src/app/reco-layout-component/idee-de-voyage/idee-de-voyage.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-idee-de-voyage',
   templateUrl: './idee-de-voyage.component.html',
   styleUrls: ['./idee-de-voyage.component.css']
 })
-export class IdeeDeVoyageComponent {
+export class IdeeDeVoyageComponent  implements OnInit{
+    idee_voyage! :Array<any>;
+  constructor() {
 
+  }
+
+
+
+
+  ngOnInit():void{
+    this.idee_voyage=[
+      {id:1,name:"Marrakech",prix:1200},
+      {id:2,name:"Tanger",prix:1200},
+      {id:3,name:"Agadir",prix:1200},
+    ]
+
+  }
 }
