b0e3e1d6b1a8247a4c7c0a952113d07db8bd87a2
nada balkassem <nada.balkassem@etu.uae.ac.ma>
2023-12-25 12:44:45 +0100

    pull

---
+++ src/app/reco-layout-component/reco-layout-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reco-layout-component',
+  templateUrl: './reco-layout-component.component.html',
+  styleUrls: ['./reco-layout-component.component.css']
+})
+export class RecoLayoutComponentComponent {
+
+}
