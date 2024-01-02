bddc071d8bee232d47ac94afbb951af6656bacf0
nada balkassem <nada.balkassem@etu.uae.ac.ma>
2024-01-02 22:02:24 +0100

    h

--- src/app/auth-layout/login/login.component.ts
+++ src/app/auth-layout/login/login.component.ts
@@ -1,13 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+
 export class LoginComponent {
-submitForm() {
-throw new Error('Method not implemented.');
+
+  // email: string = '';
+  // password: string = '';
+
+  // constructor(private http: HttpClient) {}
+
+  // login() {
+  //   const credentials = { email: this.email, password: this.password };
+
+  //   this.http.post('http://your-backend-api/login', credentials)
+  //     .subscribe(response => {
+  //       // Handle successful login, e.g., store token, redirect, etc.
+  //       console.log('Login successful', response);
+  //     }, error => {
+  //       // Handle login error, e.g., show error message
+  //       console.error('Login failed', error);
+  //     });
+  // }
 }
 
-}
