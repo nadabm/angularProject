074366b5fb467d738ac92562259430256ed9de12
ferdaous <echahbouniferdaous@gmail.com>
2024-01-24 23:44:19 +0100

    jo

--- src/app/match-layout/matches/matches.component.ts
+++ src/app/match-layout/matches/matches.component.ts
@@ -12,35 +12,43 @@ export class MatchesComponent   {
   constructor(private matchesService: MatchesService) {}
       // Dans votre composant ou service
 matchesGroup1: any[] = [
-  { team1: 'Argentine', team2: 'France', score: '2-1', date: '2024-01-23', time: '18:00', stadium: 'Stade A' },
+  { team1: 'Argentine', team2: 'France', score: '2-1', date: '2024-01-18', time: '18:00', stadium: 'Stade A' },
   // ... autres matches pour le groupe 1
 ];
 matchesGroup2: any[] = [
-  { team3: 'Morocco', team4: 'Croatia', score: '3-2', date: '2024-01-24', time: '20:00', stadium: 'Stade B' },
+  { team3: 'Morocco', team4: 'Croatia', score: '3-2', date: '2024-01-20', time: '20:00', stadium: 'Stade B' },
   // ... autres matches pour le groupe 2
 ];
 matchesGroup3: any[] = [
-  { team3: 'Morocco', team4: 'Croatia', score: '3-2', date: '2024-01-24', time: '20:00', stadium: 'Stade B' },
+  { team3: 'FRANCE', team4: 'POLAND', score: '3-1', date: '2024-01-24', time: '18:00', stadium: 'Stade B' },
   // ... autres matches pour le groupe 2
 ];
 matchesGroup4: any[] = [
-  { team3: 'Morocco', team4: 'Croatia', score: '3-2', date: '2024-01-24', time: '20:00', stadium: 'Stade B' },
+  { team3: 'NETHERLANDS', team4: 'UNITED STATES', score: '3-1', date: '2024-02-03', time: '16:00', stadium: 'Stade B' },
   // ... autres matches pour le groupe 2
 ];
 matchesGroup5: any[] = [
-  { team3: 'Morocco', team4: 'Croatia', score: '3-2', date: '2024-01-24', time: '20:00', stadium: 'Stade B' },
+  { team3: 'ENGLAND', team4: 'SENEGAL', score: '3-0', date: '2024-02-04', time: '20:00', stadium: 'Stade B' },
   // ... autres matches pour le groupe 2
 ];
 matchesGroup6: any[] = [
-  { team3: 'Morocco', team4: 'Croatia', score: '3-2', date: '2024-01-24', time: '20:00', stadium: 'Stade B' },
+  { team3: 'Morocco', team4: 'SPAIN', score: '3-2', date: '2024-01-24', time: '20:00', stadium: 'Stade B' },
   // ... autres matches pour le groupe 2
 ];
 matchesGroup7: any[] = [
-  { team3: 'Morocco', team4: 'Croatia', score: '3-2', date: '2024-01-24', time: '20:00', stadium: 'Stade B' },
+  { team3: 'PORTUGAL', team4: 'SWITZERLAND', score: '6-1', date: '2024-02-06', time: '20:00', stadium: 'Stade B' },
   // ... autres matches pour le groupe 2
 ];
 matchesGroup8: any[] = [
-  { team3: 'Morocco', team4: 'Croatia', score: '3-2', date: '2024-01-24', time: '20:00', stadium: 'Stade B' },
+  { team3: 'ENGLAND', team4: 'SENEGAL', score: '3-0', date: '2024-02-06', time: '20:00', stadium: 'Stade B' },
+  // ... autres matches pour le groupe 2
+];
+matchesGroup9: any[] = [
+  { team3: 'NETHERLANDS', team4: 'UNITED STATES', score: '3-1', date: '2024-02-06', time: '20:00', stadium: 'Stade B' },
+  // ... autres matches pour le groupe 2
+];
+matchesGroup10: any[] = [
+  { team3: 'ARGENTINA', team4: 'AUSTRALIA', score: '2-1', date: '2024-02-06', time: '20:00', stadium: 'Stade B' },
   // ... autres matches pour le groupe 2
 ];
 
--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -17,12 +17,50 @@ export class PlayersComponent implements OnInit {
   players: any[] = [];
 
   constructor(private route: ActivatedRoute, private serviceService: ServiceService) {}
-  cardsData = [
+  cardsDataGoalkeeper = [
     { index: 1, title: '', text: 'yassine BOUNOU' ,imageUrl: '/assets/photosdesjoueurs/Yassine-Bounou.jpg'},
     { index: 2, title: '', text: 'monir EL KAJOUI' ,imageUrl: '/assets/photosdesjoueurs/monir EL KAJOUI.jpg'},
     { index: 3, title: '', text: 'ahmed TAGNAOUTI' ,imageUrl: '/assets/photosdesjoueurs/ahmed TAGNAOUTI.jpg'},
     // ... ajoutez d'autres données pour chaque carte
   ];
+  cardsData2 = [
+    { index: 4, title: '', text: 'achraf HAKIMI' ,imageUrl: '/assets/photosdesjoueurs/hakimi.jpg'},
+    { index: 5, title: '', text: 'Noussair MAZRAOUI' ,imageUrl: '/assets/photosdesjoueurs/noussair.jpeg'},
+    { index: 6, title: '', text: 'Nayef AGUERD' ,imageUrl: '/assets/photosdesjoueurs/nayef.jpeg'},
+    { index: 7, title: '', text: 'Romain SAISS' ,imageUrl: '/assets/photosdesjoueurs/romain.webp'},
+    { index: 8, title: '', text: 'Jawad EL YAMIQ' ,imageUrl: '/assets/photosdesjoueurs/jawad.webp'},
+    { index: 9, title: '', text: 'Achraf DARI ' ,imageUrl: '/assets/photosdesjoueurs/achraf.jpg'},
+    { index: 10, title: '', text: 'Badr BENOUN' ,imageUrl: '/assets/photosdesjoueurs/badr.jpeg'},
+
+    // ... ajoutez d'autres données pour chaque carte
+  ];
+  cardsDataMidfielder = [
+    { index: 1, title: ' Midfielder 1', text: 'sofyan AMRABAT ', imageUrl: '/assets/photosdesjoueurs/amrabet.jpg'},
+    { index: 2, title: ' Midfielder 2', text: 'Hakim ZIYECH', imageUrl: '/assets/photosdesjoueurs/hakim.jpg'},
+    { index: 3, title: ' Midfielder 3', text: 'Azzedine OUNAHI', imageUrl: '/assets/photosdesjoueurs/ounahi.jfif'},
+    { index: 4, title: 'Midfielder 4', text: 'Anas ZAROURY', imageUrl: '/assets/photosdesjoueurs/anass.jpeg'},
+    { index: 5, title: ' Midfielder 5', text: 'ilias CHAIR ', imageUrl: '/assets/photosdesjoueurs/ilias.webp'},
+    { index: 6, title: ' Midfielder 6', text: 'Zakaria ABOUKHLAL', imageUrl: '/assets/photosdesjoueurs/zakaria.jpg'},
+    { index: 7, title: ' Midfielder 7', text: 'Selim AMALLAH', imageUrl: '/assets/photosdesjoueurs/selim.jpg'},
+    { index: 8, title: ' Midfielder 8', text: 'Sofiane BOUFAL', imageUrl: '/assets/photosdesjoueurs/boufal.jfif'},
+    { index: 9, title: ' Midfielder 9', text: ' Bilal EL KHANNOUSS', imageUrl: '/assets/photosdesjoueurs/Bilal.jpeg'},
+    // ... ajoutez d'autres données pour chaque carte de la catégorie "Midfielder"
+  ];
+  cardsDataForward = [
+    { index: 1, title: ' ', text: 'hamdallah abderrazak ', imageUrl: '/assets/photosdesjoueurs/hamdallah.jfif'},
+    { index: 2, title: ' abdelhamid sabiri', text: 'abdelhamid sabiri', imageUrl: '/assets/photosdesjoueurs/sabiri.jpg'},
+    { index: 3, title: ' Midfielder 3', text: 'abdessamad ezzalzouli', imageUrl: '/assets/photosdesjoueurs/abdessamad.png'},
+    { index: 4, title: 'Midfielder 4', text: 'Youssef EN NESYRI', imageUrl: '/assets/photosdesjoueurs/nesyri.jpg'},
+    { index: 5, title: ' Midfielder 5', text: 'Walid CHEDDIRA ', imageUrl: '/assets/photosdesjoueurs/walid.jpg'},
+
+    // ... ajoutez d'autres données pour chaque carte de la catégorie "Midfielder"
+  ];
+
+cardsDataManager = [
+  { index: 1, title: 'Walid REGRAGUI', text: 'Description 1', imageUrl: '/assets/photosdesjoueurs/regragui.webp'},
+  // ... ajoutez d'autres données pour chaque carte de la catégorie "Manager"
+];
+
 
   ngOnInit(): void {
   // Récupérer l'ID de l'équipe à partir de l'URL
